f3673877dc79f9673b636aa15af0cfe2d2ba0cbb
MikeArms IVRE <mike@immersivevreducation.com>
2018-04-20 17:39:41 +0100

    added locked in seat back to sit trigger

--- Assets/ENGAGE_SceneCreator/Scripts/Engine/LVR_SitTrigger.cs
+++ Assets/ENGAGE_SceneCreator/Scripts/Engine/LVR_SitTrigger.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class LVR_SitTrigger : MonoBehaviour {
 	public Transform floorCollider;
 	public float lowerHeadAmount = 0.6f;
+    public bool lockedInSeat = false;
 
 }
