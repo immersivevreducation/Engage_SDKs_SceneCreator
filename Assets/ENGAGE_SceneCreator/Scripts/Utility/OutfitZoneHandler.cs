ae4ab19f3d999dadd30405cd9b5d4e2e741c577b
james-ivre <james@immersivevreducation.com>
2020-02-17 10:50:55 +0000

    Add OutfitZoneHandler script

---
+++ Assets/ENGAGE_SceneCreator/Scripts/Utility/OutfitZoneHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handler class for OutfitZones, attach script to GameObject in scene,
+/// assign a Collider from each zone to the script, and assign an associated override value
+/// to each collider.
+/// </summary>
+public class OutfitZoneHandler : MonoBehaviour
+{
+    //Public variables
+    [Header("Colliders for each zone")]
+    public List<GameObject> zones;
+    [Header("Override value for each zone in order")]
+    [Tooltip("-1 for no outfit, 0 for spacesuit")]
+    public List<int> overrideValues;
+    [Header("Override value for if player isn't in a zone")]
+    public int defaultZoneOverride;
+}
