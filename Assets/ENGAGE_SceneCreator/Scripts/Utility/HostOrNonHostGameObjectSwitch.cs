fb58072960128adf6904c3d27b92565dd23edcd1
MikeArms IVRE <mike@immersivevreducation.com>
2020-02-21 12:09:47 +0000

    Host/NonHost script

---
+++ Assets/ENGAGE_SceneCreator/Scripts/Utility/HostOrNonHostGameObjectSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostOrNonHostGameObjectSwitch : MonoBehaviour
+{
+    public GameObject[] host_OnlyObjects;
+    public GameObject[] non_HostObjects;
+
+    float timeDelay = 0.5f;
+    float timeCounter = 0;
+
+#if UNITY_ENGAGE
+    private void Update()
+    {
+        if (timeCounter < timeDelay)
+        {
+            timeCounter += Time.deltaTime;
+            return;
+        }
+
+        timeCounter = 0;
+
+        if (Engage_NetworkController.instance.isSessionHost)
+        {
+            foreach (GameObject obj in host_OnlyObjects)
+                if (obj != null)
+                    if(!obj.activeSelf)
+                        obj.SetActive(true);
+            foreach (GameObject obj in non_HostObjects)
+                if (obj != null)
+                    if (obj.activeSelf)
+                        obj.SetActive(false);
+        }
+        else
+        {
+            foreach (GameObject obj in non_HostObjects)
+                if (obj != null)
+                    if (!obj.activeSelf)
+                        obj.SetActive(true);
+            foreach (GameObject obj in host_OnlyObjects)
+                if (obj != null)
+                    if (obj.activeSelf)
+                        obj.SetActive(false);
+        }
+    }
+#endif
+}
