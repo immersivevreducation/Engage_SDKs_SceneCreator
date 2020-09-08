8224109ec7a6e3ce4718b8b5c36f87ca48d78211
james-ivre <james@immersivevreducation.com>
2020-09-08 15:42:27 +0100

    Added WebClient updater

--- Assets/Editor/UpdateManager.cs
+++ Assets/Editor/UpdateManager.cs
@@ -2,6 +2,9 @@
 using UnityEditor;
 using System.Collections;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System;
 
 namespace AssetBundles
 {
@@ -10,6 +13,7 @@ namespace AssetBundles
         bool checkComplete = false;
         UnityEditor.PackageManager.Requests.AddRequest sdkUpdateRequest;
         string packageID = "com.ivre.engage_scenecreator_sdk";
+        string _url = "SceneCreatorSDK.unitypackage";
 
         [MenuItem("SDK/Check for updates")]
         public static void ShowUpdateWindow()
@@ -21,14 +25,36 @@ namespace AssetBundles
         {
             if (GUILayout.Button("Check for updates"))
             {
-                sdkUpdateRequest = UnityEditor.PackageManager.Client.Add(packageID);
                 checkComplete = true;
+                ImportPackage();
             }
             EditorGUILayout.Space();
 
             if (checkComplete)
-                if (sdkUpdateRequest.IsCompleted)
-                    Debug.Log("SDK update complete with result of: " + sdkUpdateRequest.Error.message);
+            {
+            }
+
+        }
+
+        private void ImportPackage()
+        {
+            WebClient wc = new WebClient();
+            Uri _uri = new Uri("https://github.com/immersivevreducation/Engage_SDKs_SceneCreator/blob/master/engage_scenecreator_sdk.unitypackage?raw=true");
+            wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(_uri, "SceneCreatorSDK");
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (File.Exists("SceneCreatorSDK.unitypackage"))
+                FileUtil.DeleteFileOrDirectory("SceneCreatorSDK.unitypackage");
+            FileUtil.MoveFileOrDirectory("SceneCreatorSDK", "SceneCreatorSDK.unitypackage");
+            Debug.Log("Package download completed");
+            if (File.Exists(_url))
+            {
+                Debug.Log("Package exists");
+                AssetDatabase.ImportPackage(_url, false);
+            }
         }
     }
 }
