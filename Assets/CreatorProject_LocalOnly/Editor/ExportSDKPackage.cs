d9f297337ec8be52cdbd7e444a5e99d9fb9dc4cd
MikeArms IVRE <mike@immersivevreducation.com>
2020-10-05 16:13:32 +0100

    Update Editor Folder (Prepare for Tyler Tool)

--- Assets/CreatorProject_LocalOnly/Editor/ExportSDKPackage.cs
+++ Assets/CreatorProject_LocalOnly/Editor/ExportSDKPackage.cs
@@ -7,7 +7,7 @@ public static class ExportWithLayers {
 	    [MenuItem("ENGAGE/Export SDK with tags and physics layers")]
 	    public static void ExportPackage()
 	    {
-		        string[] projectContent = new string[] {"Assets/Engage_CreatorSDK","Assets/Standard Assets","ProjectSettings/TagManager.asset"};
+		        string[] projectContent = new string[] {"Assets/Editor", "Assets/Engage_CreatorSDK","Assets/Standard Assets","ProjectSettings/TagManager.asset"};
 				AssetDatabase.ExportPackage(projectContent, "CreatorSDK.unitypackage", ExportPackageOptions.Recurse); 
 		        Debug.Log("Project Exported");
 		    }
