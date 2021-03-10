a60dd4055ab051584a91bea9d3fb3bcfbd4f0f19
Tylerlnch <tlynch@immersivevreducation.com>
2021-03-10 17:24:24 +0000

    Test update with versioning

--- Assets/CreatorProject_LocalOnly/Editor/CreateSDKUpdate.cs
+++ Assets/CreatorProject_LocalOnly/Editor/CreateSDKUpdate.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System;
+using System.IO;
 public class CreateSDKUpdate : EditorWindow
 {
     string  projectFolder = Application.dataPath.Replace("/Assets", "");
@@ -7,6 +9,31 @@ public class CreateSDKUpdate : EditorWindow
     [MenuItem("ENGAGE/Create SDK Update")]
     static void CreateUpdateZip()
     {
+        string currentSDKVersion = null;
+        int versionNumber = 0;
+        string path1 = Application.dataPath+@"/ENGAGE_CreatorSDK/SDKUpdateVersion.txt";
+        if (File.Exists(path1))
+        {
+            currentSDKVersion = System.IO.File.ReadAllText(path1);
+        }
+        if (string.IsNullOrEmpty(currentSDKVersion))
+        {
+            using (StreamWriter writer = new StreamWriter(path1))  
+            {  
+                writer.Write("0");  
+            }
+        }
+        else
+        {
+            versionNumber = Int32.Parse(currentSDKVersion);
+            versionNumber +=1;
+            using (StreamWriter writer = new StreamWriter(path1))  
+            {  
+                writer.Write(versionNumber.ToString());  
+            }
+        }
+            
+        
         lzip.compressDir(Application.dataPath+"\\ENGAGE_CreatorSDK",1,Application.dataPath.Replace("/Assets", "")+"/CreatorSDKUpdate.zip");
         Debug.Log("Update Created");
     }
