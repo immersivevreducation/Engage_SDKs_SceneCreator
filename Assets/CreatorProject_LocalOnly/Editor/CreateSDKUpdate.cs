e3dcf040d2763e489a63b448115067d01aba8090
Tylerlnch <tlynch@immersivevreducation.com>
2021-03-10 10:43:02 +0000

    Creating a test update

---
+++ Assets/CreatorProject_LocalOnly/Editor/CreateSDKUpdate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+public class CreateSDKUpdate : EditorWindow
+{
+    string  projectFolder = Application.dataPath.Replace("/Assets", "");
+ 
+    [MenuItem("ENGAGE/Create SDK Update")]
+    static void CreateUpdateZip()
+    {
+        lzip.compressDir(Application.dataPath+"\\ENGAGE_CreatorSDK",1,Application.dataPath.Replace("/Assets", "")+"/CreatorSDKUpdate.zip");
+        Debug.Log("Update Created");
+    }
+}
+
+
+
+
