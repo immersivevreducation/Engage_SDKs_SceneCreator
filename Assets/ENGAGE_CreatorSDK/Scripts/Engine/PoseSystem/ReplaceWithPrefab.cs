1560039a676b1310b9a2fc873caf190ace930754
Bryn LaFollette <bryn@immersivevreducation.com>
2021-06-25 14:28:57 +0200

    Adds additional fields for controlling behavior of replacement of selected game objects.

--- Assets/ENGAGE_CreatorSDK/Scripts/Engine/PoseSystem/ReplaceWithPrefab.cs
+++ Assets/ENGAGE_CreatorSDK/Scripts/Engine/PoseSystem/ReplaceWithPrefab.cs
@@ -6,6 +6,14 @@ using UnityEditor;
 public class ReplaceWithPrefab : EditorWindow
 {
     [SerializeField] private GameObject prefab;
+    [SerializeField] private bool applyPosition = true;
+    [SerializeField] private bool applyRotation = true;
+    [SerializeField] private bool applyScale = true;
+
+    [SerializeField] private bool offsetOptions;
+    [SerializeField] private Vector3 offsetPosition;
+    [SerializeField] private Vector3 offsetRotation;
+    [SerializeField] private Vector3 offsetScale;
 
     [MenuItem("Tools/Replace With Prefab")]
     static void CreateReplaceWithPrefab()
@@ -17,6 +25,22 @@ public class ReplaceWithPrefab : EditorWindow
     {
         prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), false);
 
+        applyPosition = EditorGUILayout.Toggle("Apply Position", applyPosition);
+        applyRotation = EditorGUILayout.Toggle("Apply Rotation", applyRotation);
+        applyScale = EditorGUILayout.Toggle("Apply Scale", applyScale);
+
+        offsetOptions = EditorGUILayout.BeginFoldoutHeaderGroup(offsetOptions, "Offset Transform");
+
+        if (offsetOptions)
+        {
+            offsetPosition = EditorGUILayout.Vector3Field("Offset Position", offsetPosition);
+            offsetRotation = EditorGUILayout.Vector3Field("Offset Rotation", offsetRotation);
+            offsetScale = EditorGUILayout.Vector3Field("Offset Scale", offsetScale);
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+
         if (GUILayout.Button("Replace"))
         {
             var selection = Selection.gameObjects;
@@ -24,10 +48,10 @@ public class ReplaceWithPrefab : EditorWindow
             for (var i = selection.Length - 1; i >= 0; --i)
             {
                 var selected = selection[i];
-                var prefabType = PrefabUtility.GetPrefabType(prefab);
+                var prefabType = PrefabUtility.GetPrefabAssetType(prefab);
                 GameObject newObject;
 
-                if (prefabType == PrefabType.Prefab)
+                if (prefabType != PrefabAssetType.NotAPrefab)
                 {
                     newObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
                     newObject.name = prefab.name + " (" + i + ")";
@@ -45,10 +69,12 @@ public class ReplaceWithPrefab : EditorWindow
                 }
 
                 Undo.RegisterCreatedObjectUndo(newObject, "Replace With Prefabs");
-                newObject.transform.parent = selected.transform.parent;
-                newObject.transform.localPosition = selected.transform.localPosition;
-                newObject.transform.localRotation = selected.transform.localRotation;
-                newObject.transform.localScale = selected.transform.localScale;
+                newObject.transform.SetParent(selected.transform.parent);
+
+                newObject.transform.localPosition = (applyPosition ? selected.transform.localPosition : Vector3.zero) + offsetPosition;
+                newObject.transform.localRotation = Quaternion.Euler((applyRotation ? selected.transform.localRotation.eulerAngles : Vector3.zero) + offsetRotation);
+                newObject.transform.localScale = (applyScale ? selected.transform.localScale : Vector3.one) + offsetScale;
+
                 newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
                 Undo.DestroyObjectImmediate(selected);
             }
