aa9a45513ab540a7031552a638303e55a66957f6
MikeArmstrong24 <mike@immersivevreducation.com>
2021-07-12 23:28:21 +0100

    Added Network Object Interface Example and new utility script

--- Assets/ENGAGE_CreatorSDK/Scripts/Engine/Engage_CreateNetworkObjectFromSceneObject.cs
+++ Assets/ENGAGE_CreatorSDK/Scripts/Engine/Engage_CreateNetworkObjectFromSceneObject.cs
@@ -11,12 +11,51 @@ public class Engage_CreateNetworkObjectFromSceneObject : MonoBehaviour
     [Header("This objects scale will always be (1,1,1)", order = 4)]
     [Header("",order = 5)]
 
-    [Header("Unique name for Objects, this must match GO Name", order = 6)]
+    /// <summary>Unique object name (make sure this is scene-unique)</summary>
     public string veryUniqueObjectName;
+
+    /// <summary>Enable gravity on rigidbody?</summary>
     public bool gravityEnabled;
+
+    /// <summary>Should this object always be kinematic? (no physics)</summary>
     public bool alwaysKinematic;
 
-    [Header("Advanced Functionality", order = 7)]
+    /// <summary>String that can be applied and used for interactive games</summary>
     public string optionalStringForGames;
+
+    /// <summary>Default is to use Bones tag</summary>
     public bool dontChangeTag;
+
+    /// <summary>Override for Ridigbody's Mass</summary>
+    public float rigidBodyMass = 1.0f;
+
+    /// <summary>Override for Ridigbody's Drag</summary>
+    public float rigidBodyDrag = 0.0f;
+
+    /// <summary>Override for Ridigbody's Angular Drag</summary>
+    public float rigidBodyAngularDrag = 0.05f;
+
+
+
+    #region Network Object manual interface 
+
+    /// <summary>
+    /// Update transform value manually. This should be done rarely (via button / etc) by one user if possible.
+    /// Object will be dropped if it is held when function is called.
+    /// </summary>
+    public void UpdatePositionManualViaReference(Transform positionReferenceTransform){}
+
+    public void UpdateRotationManualViaReference(Transform rotationReferenceTransform){}
+
+    public void UpdateScaleManualViaReference(Transform scaleReferenceTransform){}
+
+    public void UpdateAllTransformValuesManualViaReference(Transform referenceTransform){}
+
+    public void SetKinematicDefault(bool alwaysKinematic){}
+
+    public void SetGravityEnabledDefault(bool gravityEnabled){}
+
+    public void SetIsGrabbable(bool grabbable){}
+
+    #endregion
 }
